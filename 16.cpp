38c3140c93c7e90fb57c3fd8a2566901fc3220e7
alxest <youngju.song@sf.snu.ac.kr>
2015-08-10 03:59:17 +0900

    Implement 16.

--- 16.cpp
+++ 16.cpp
@@ -8,8 +8,19 @@ void Divide(int x, int y, int *result, int *remainder) {
 		*remainder = x % y;
 }
 
+void ParamArray(int length, ...) {
+		va_list args;
+  va_start(args, length);
+		cout << length << endl;
+		for(int i=0; i<length; i++) {
+				cout << va_arg(args, int) << " ";
+		}
+		cout << endl;
+}
+
 int main() {
 		int res, rem;
 		Divide(10, 3, &res, &rem);
 		cout << res << " " << rem << endl;
+		ParamArray(4,5,2,6,8);
 }
--- 16.cs
+++ 16.cs
@@ -4,8 +4,15 @@ class Test {
 								result = x / y;
 								remainder = x % y;
 				}
+				static void ParamArray(params int[] args) {
+								Console.WriteLine(args.Length);
+								for(int i=0; i<args.Length; i++)
+												Console.Write(args[i] + " ");
+								Console.WriteLine();
+				}
 				static void Main() {
 								int res, rem;
 								Divide(10, 3, out res, out rem); Console.WriteLine("{0} {1}", res, rem);
+								ParamArray(5,2,6,8);
 				}
 }
